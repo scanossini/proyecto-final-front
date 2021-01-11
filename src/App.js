1e80b0ae80af149d85aaa43f53d2b491f963fb0f
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-01-11 19:34:39 -0300

    boludeces

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 const App = () => {
     return (
-        <div>
-           holaa
+        <div ClassName="App">
+           <NavBar />
         </div>
     );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, {useState, Fragment} from 'react';
+
+const NavBar = () => {
+
+    return (
+        <Fragment>
+            
+        </Fragment>
+      );
+}
+ 
+export default NavBar;
