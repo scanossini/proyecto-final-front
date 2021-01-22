919a9309ed5c3102c30ef74176110a8fb944eaa4
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-01-22 17:06:08 -0300

    probando faker

--- src/pages/Donantes.js
+++ src/pages/Donantes.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import faker from 'faker'
+
 
 export const Donantes = () => {
+    const data = new Array(10000).fill().map((value, index) => ({ id: index, title: faker.lorem.words(5), body: faker.lorem.sentences(4) }))
     return (
         <div className='donantes'>
-            <h1>
-                Donantes
-            </h1>
+            {data.map(((item) => (
+                <div key={item.id} className="post">
+                    <h3>{item.title} - {item.id}</h3>
+                    <p>{item.body}</p>
+                </div>
+            )))}
+
         </div>
     )
 }
