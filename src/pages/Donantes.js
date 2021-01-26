96f9895b8325b45ccf39f79652c1bb4396173df1
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-01-25 22:07:06 -0300

    refactor paginacion

---
+++ src/pages/Donantes.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import faker from 'faker'
+import axios from 'axios';
+import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
+import { Pagination } from '../components/Pagination/Pagination'
+import { Posts } from '../components/Posts/Posts'
+
+
+export const Donantes = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  }, []);
+
+  //obtener posts actuales
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  //cambiar de pagina
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3"> Donantes </h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination 
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+         paginate={paginate}/>
+    </div>
+  );
+}
+
