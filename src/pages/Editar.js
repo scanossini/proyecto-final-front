085a2f361fa6dee890da2aa5460e8242c1c1f170
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-02-11 22:39:42 -0300

    el emma se mata si ve como saque el id en editar

--- src/pages/Editar.js
+++ src/pages/Editar.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export const Editar = () => {
+export function Editar(props) {
+    const id = props.location.pathname.split('/').slice(4,5).join('/');
     return (
         <h1 className='home'>
-            Editar
+            Editar {id}
         </h1>
     )
 }
