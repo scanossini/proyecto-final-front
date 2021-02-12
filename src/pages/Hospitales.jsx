e7da6db39b26478c2ff5f6614f14c47cd3c070f8
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-02-11 21:27:18 -0300

    asd

--- src/pages/Hospitales.jsx
+++ src/pages/Hospitales.jsx
@@ -1,4 +1,4 @@
-import { Paper, Container, Fab, Table, TableCell, TableHead, Tooltip, TableContainer, TableBody, TableRow, IconButton } from '@material-ui/core';
+import { Paper, makeStyles, Container, Fab, Table, TableCell, TableHead, Tooltip, TableContainer, TableBody, TableRow, IconButton } from '@material-ui/core';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import EditIcon from '@material-ui/icons/Edit';
@@ -6,7 +6,36 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from "@material-ui/icons/Add";
 import { useHistory } from 'react-router-dom';
 
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650
+    },
+    head: {
+        backgroundColor: "lightblue",
+    },
+    button: {
+        position: "fixed",
+        right: "100px",
+        bottom: "40px",
+    },
+    container: {
+        maxHeight: 440,
+        marginTop: 70
+    },
+    editButton: {
+        backgroundColor: "rgb(230,230,230)",
+        fontSize: 15,
+    },
+    deleteButton: {
+        backgroundColor: "rgb(230,230,230)",
+        fontSize: 15,
+        marginLeft: 5, 
+    }
+})
+
+
 export const Hospitales = () => {
+    const classes = useStyles();
     const history = useHistory();
     const [hospitales, setHospitales] = useState("");
 
@@ -38,12 +67,15 @@ export const Hospitales = () => {
                                         <TableCell>{hospital.nombre}</TableCell>
                                         <TableCell>
                                             <Tooltip title="Editar" aria-label="editar">
-                                                <IconButton >
+                                                <IconButton
+                                                    className={classes.editButton} >
                                                 <EditIcon color="action" />
                                                 </IconButton>
                                             </Tooltip>
                                             <Tooltip title="Eliminar" aria-label="eliminar">
-                                                <IconButton >
+                                                <IconButton
+                                                   className={classes.deleteButton}
+                                                >
                                                 <DeleteIcon color="secondary" />
                                                 </IconButton>
                                             </Tooltip>
