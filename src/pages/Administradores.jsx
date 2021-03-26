497ac15f357e8dd8a78a96aa4518e9f4cc4ee1b2
Juan Martin Giron <jgiron@hexacta.com>
2021-03-26 11:28:35 -0300

    gestion de admins

--- src/pages/Administradores.jsx
+++ src/pages/Administradores.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@material-ui/core'
+import { Button, Container, FormControl, Grid, IconButton, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip, Typography } from '@material-ui/core'
 import { useSnackbar } from 'notistack';
+import DeleteIcon from '@material-ui/icons/Delete';
 import swal from 'sweetalert';
 import { Spinner } from '../components/Spinner/Spinner';
 
 export const Administradores = () => {
     const [admin, setAdmin] = useState("");
     const [hospitales, setHospitales] = useState([]);
+    const [admins, setAdmins] = useState([]);
     const [hospital, setHospital] = useState("");
     const [nombre, setNombre] = useState("");
     const [password, setPassword] = useState("");
@@ -24,6 +26,9 @@ export const Administradores = () => {
             .catch((error) => {
                 console.log(error);
         })
+
+        axios.get('http://localhost:5000/admin')
+            .then((response) => setAdmins(response.data))
     }, [])
 
     const handleCreate = () => {
@@ -34,7 +39,26 @@ export const Administradores = () => {
         } 
 
         axios.post("http://localhost:5000/admin/signup", data)
-            .then(res => enqueueSnackbar("Administrador agregado", {variant: "success"}))
+            .then(res => {
+                var data = res.data
+                var newAdmins = admins.concat(data)
+                setAdmins(newAdmins)
+                enqueueSnackbar("Administrador agregado", {variant: "success"})
+            })
+            .catch(err => swal(err.response.data, "", "error"))
+    }
+    
+    const nombreHospital = (id) => {
+            const hospital = hospitales.filter(hospital => hospital._id === id)
+            return hospital[0].nombre
+    }
+
+    const handleDelete = (id) => {
+        axios.delete("http://localhost:5000/admin/" + id, {"headers": {"token": sessionStorage.getItem("token")}})
+            .then(res => { 
+                setAdmins(admins.filter(admin => admin._id !== id))
+                enqueueSnackbar("Administrador eliminado", {variant: "success"}) 
+            })
             .catch(err => swal(err.response.data, "", "error"))
     }
 
@@ -91,6 +115,34 @@ export const Administradores = () => {
                             Crear
                         </Button>
                     </div>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Administrador</TableCell>
+                                    <TableCell>Hospital</TableCell>
+                                    <TableCell>Eliminar</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    admins.map((admin) => (
+                                        <TableRow>
+                                            <TableCell>{admin.nickname}</TableCell>
+                                            <TableCell>{nombreHospital(admin.hospital)}</TableCell>
+                                            <TableCell>
+                                                <Tooltip title="Eliminar">
+                                                    <IconButton onClick={() => handleDelete(admin._id)}>
+                                                        <DeleteIcon color="secondary" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Container>
             : <h1>Esto solo puede ser accedido por el administrador general</h1>
         : <Spinner />
